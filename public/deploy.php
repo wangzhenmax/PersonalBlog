b1afdb7ef1b7418327f66c473846ed9ca3cc3e3e
wangzhen <457340@qq.com>
2018-11-30 19:51:37 +0800

    'test'

--- public/deploy.php
+++ public/deploy.php
@@ -1,3 +1,2 @@
 <?php
-ini_set ("disable_functions","phpinfo,eval,passthru,system,chroot,scandir,chgrp,chown,shell_exec,proc_open,proc_get_status,ini_alter,ini_alter,ini_restore,dl,pfsockopen,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server,fsocket,fsockopen");
  exec('get pull');
